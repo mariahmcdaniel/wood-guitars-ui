da3ce4956af6c4fe793f178c56814a542a1fa908
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-04-29 18:46:31 -0400

    head, index.js edits

--- components/layout.js
+++ components/layout.js
@@ -2,37 +2,26 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Nav from './nav';
 
-// const name = 'Eric Wood | Master Luthier';
 
 export const siteTitle = 'Eric-Wood-Guitars';
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
      <div >
       <Head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Eric Wood Guitars</title>
+
         <Link rel="icon" href="/favicon.ico" />
-        {/* <meta
-          name="description"
-          content=""
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" /> */}
+       
       </Head>
       <Nav />
       <div className="container self-center mb-40">
       
       <main>{children}</main>
-      {/* {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      )} */}
+
       </div>
       </div>
   );
