370c22220438fb4dc22de92c53fa6d8d1a5df179
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-04-29 19:47:24 -0400

    imported css

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Layout from '@/components/layout'
+import utilStyles from '@/styles/utils.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
