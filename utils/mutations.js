13545166e9fac00a3a2ac9ead194a52ed63d0382
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-10 18:36:28 -0400

    register user mutation

--- utils/mutations.js
+++ utils/mutations.js
@@ -12,3 +12,15 @@ export const LOGIN_USER = gql`
   }
 }
 `;
+
+export const REGISTER_USER = gql`
+  mutation addUser($email: String!, $username: String!, $password: String!, $age: String, $city: String, $state: String, $interestedIn: String, $gender: String, $pronouns: String, $firstName: String!, $lastName: String!) {
+  addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+`;
