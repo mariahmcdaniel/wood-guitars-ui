75d792d77e4757de10ad5972a242afe13fcfc9f6
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-10 18:19:31 -0400

    registration form

---
+++ pages/register.js
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+   
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName", { 
+        required: <p style={{ color: "red" }}>Name fields are required</p>, 
+        maxLength: 20, 
+        pattern: {
+        value: /^[\p{L} ,.'-]+$/u,
+        message: <p style={{ color: "red" }}>Please ensure that you have entered a valid first name.</p>
+        }
+      })} />
+      <input {...register("lastName", { 
+        required: <p style={{ color: "red" }}>Name fields are required</p>, 
+        maxLength: 20, 
+        pattern: {
+        value: /^[\p{L} ,.'-]+$/u,
+        message: <p style={{ color: "red" }}>Please ensure that you have entered a valid last name.</p>
+        }
+      })} />
+      <input {...register("username", { 
+        required: <p style={{ color: "red" }}>Please enter a username.</p>, 
+        maxLength: 20, 
+        pattern: {
+        value: /^[a-zA-Z0-9.\_]{5,16}$/,
+        message: <p style={{ color: "red" }}>Username must contain 5-16 characters and no spaces. Only letters and . (period) or _ (underscore) are accepted.</p>
+        }
+      })} />
+      <input {...register("email", {
+        required: <p style={{ color: "red" }}>email is required for registration</p>, 
+        pattern: { 
+          value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        message: <p style={{ color: "red" }}>Please enter a valid email address</p> 
+              }
+        })} />
+
+      <input type="submit" />
+    </form>
+  );
+}
