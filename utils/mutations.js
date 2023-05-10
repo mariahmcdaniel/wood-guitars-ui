7d3fe5c162e37835df5f03fd9b1b50661915c26a
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-10 18:40:33 -0400

    register user mutation

--- utils/mutations.js
+++ utils/mutations.js
@@ -3,18 +3,18 @@ import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($username: String!, $password: String!) {
-  login(email: $email, password: $password) {
+  login(username: $username, password: $password) {
     token
     user {
       _id
-      email
+      username
     }
   }
 }
 `;
 
 export const REGISTER_USER = gql`
-  mutation addUser($email: String!, $username: String!, $password: String!, $age: String, $city: String, $state: String, $interestedIn: String, $gender: String, $pronouns: String, $firstName: String!, $lastName: String!) {
+  mutation addUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!) {
   addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
     token
     user {
