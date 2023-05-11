e6bf169ed0a47dbd1a304ebb7719027bd9ca3a55
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-10 21:55:47 -0400

    resolve next headcount missing err

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
   return (
      <div >
       <Head>
-        <meta charset="UTF-8"/>
+        <meta charSet="UTF-8"/>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Eric Wood Guitars</title>
