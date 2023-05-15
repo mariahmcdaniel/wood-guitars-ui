36cb4d96ff0d33abae68c7111ad2bf2312f43913
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-14 21:48:49 -0400

    order pg styled components

--- pages/order.js
+++ pages/order.js
@@ -1,10 +1,20 @@
 import Layout from "@/components/layout"
+import styled from "styled-components";
+
+const H1 = styled.h1`
+font-size: clamp(1.4rem, 4vw, 1.8rem);
+color: #E6FCD6;
+display: flex;
+justify-content:center;
+padding: calc(10px + 2vw);
+text-align: center;
+`; 
 
 export default function Order(){
   return(
     <Layout>
       <div>
-      <h1>Purchase a custom instrument</h1>
+      <H1>Purchase a custom instrument</H1>
       </div>
       <section>
       <h2>While the ordering system is currently being built- please reach out via e-mail (ericwood208@gmail.com) or instagram (@ericwoodguitars) for more information about purchasing an instrument.</h2>
