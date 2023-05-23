191d94b8124f511cc3c53d36fe26d81236d91c99
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-23 19:53:30 -0400

    staged for deploy

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:1337/graphql",
+    uri: "https://eric-wood-guitars-backend.herokuapp.com/graphql",
     cache: new InMemoryCache(),
 });
 
